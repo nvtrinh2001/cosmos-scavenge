21332aade555c0c4ef11acb0570358d1e887f368
Trinh V. Nguyen <nguyenvantrinh2607@gmail.com>
2022-10-31 01:36:08 +0700

    implement tx methods that make transactions containing messages

--- x/scavenge/client/cli/tx_submit_scavenge.go
+++ x/scavenge/client/cli/tx_submit_scavenge.go
@@ -1,46 +1,50 @@
+// x/scavenge/client/cli/tx_submit_scavenge.go
+
 package cli
 
 import (
-	"strconv"
+    "crypto/sha256"
+    "encoding/hex"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/client/tx"
-	"github.com/spf13/cobra"
-	"scavenge/x/scavenge/types"
+    "github.com/cosmos/cosmos-sdk/client"
+    "github.com/cosmos/cosmos-sdk/client/flags"
+    "github.com/cosmos/cosmos-sdk/client/tx"
+    "github.com/spf13/cobra"
+
+    "scavenge/x/scavenge/types"
 )
 
-var _ = strconv.Itoa(0)
-
 func CmdSubmitScavenge() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "submit-scavenge [solution-hash] [description] [reward]",
-		Short: "Broadcast message submit-scavenge",
-		Args:  cobra.ExactArgs(3),
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argSolutionHash := args[0]
-			argDescription := args[1]
-			argReward := args[2]
+    cmd := &cobra.Command{
+        Use:   "submit-scavenge [solution] [description] [reward]",
+        Short: "Broadcast message submit-scavenge",
+        Args:  cobra.ExactArgs(3),
+        RunE: func(cmd *cobra.Command, args []string) error {
+            clientCtx, err := client.GetClientTxContext(cmd)
+            if err != nil {
+                return err
+            }
 
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
+            // find a hash of the solution
+            solutionHash := sha256.Sum256([]byte(args[0]))
 
-			msg := types.NewMsgSubmitScavenge(
-				clientCtx.GetFromAddress().String(),
-				argSolutionHash,
-				argDescription,
-				argReward,
-			)
-			if err := msg.ValidateBasic(); err != nil {
-				return err
-			}
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
-		},
-	}
+            // convert the hash to string
+            solutionHashString := hex.EncodeToString(solutionHash[:])
+            argsDescription := string(args[1])
+            argsReward := string(args[2])
 
-	flags.AddTxFlagsToCmd(cmd)
+            // create a new message
+            msg := types.NewMsgSubmitScavenge(clientCtx.GetFromAddress().String(), string(solutionHashString), string(argsDescription), string(argsReward))
+            if err := msg.ValidateBasic(); err != nil {
+                return err
+            }
 
-	return cmd
+            // broadcast the transaction with the message to the blockchain
+            return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+        },
+    }
+
+    flags.AddTxFlagsToCmd(cmd)
+
+    return cmd
 }
